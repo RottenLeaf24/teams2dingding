0645fcd2a47aad208affe96e9c501b72ace4a188
en.tang <en.tang@aylaasia.com>
2021-01-06 11:20:30 +0800

    Add scheduler job for synchronize messages only during non-working hours.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import getTeamsMsg
 import s2dingding
 import time
 from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
+from chinese_calendar import is_workday
+scheduler = BlockingScheduler()
 def trans_msg():
     json_msg = getTeamsMsg.getchatMsg()
 
@@ -13,6 +16,20 @@ def trans_msg():
             for msg in reversed(msg_list):
                 send_msg = "%s£º\n%s"%(chat,str(msg).strip('{').strip('}'))
                 s2dingding.s2dd(send_msg)
-while True:
-    trans_msg()
-    time.sleep(10)
+
+
+def running_plan():
+    ismsg = scheduler.get_job(job_id='msg')
+    if ismsg:
+        scheduler.remove_job(job_id='msg')
+
+    today = datetime.date.today()
+    workday = is_workday(today)
+    if workday:
+        scheduler.add_job(trans_msg, 'cron', hour='0-8,18-23', second='0,10,20,30,40,50', id='msg', max_instances=3)
+    else:
+        scheduler.add_job(trans_msg, 'cron', hour='0-23', second='0,10,20,30,40,50', id='msg', max_instances=3)
+
+scheduler.add_job(running_plan, 'cron', hour='0', id='iswork')
+running_plan()
+scheduler.start()
